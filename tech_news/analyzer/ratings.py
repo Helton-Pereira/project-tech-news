53ca1baf1e84e46aa604d0887d1d64391cb36319
Helton-Pereira <helton.bfsm@gmail.com>
2023-04-14 12:12:00 -0300

    Req 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,14 @@
+from collections import Counter
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    categories = []
+    news_in_db = find_news()
+
+    for news in news_in_db:
+        categories.append(news["category"])
+
+    result = Counter(sorted(categories))
+    return sorted(result, key=result.get, reverse=True)[0:5]
